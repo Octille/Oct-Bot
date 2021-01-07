01639eff54bedcdd9ea5bfeb3d8728885764a7dd
Gurkirat <sainigurkirat111@gmail.com>
2021-01-07 13:29:57 -0600

    rerole

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const colors = require('../colors.json');
 const {
-    prefix
+    prefix//cdcsdfcds sfsdf esdf
+
 } = require('../config.json');
 
 module.exports = {
@@ -18,7 +19,7 @@ module.exports = {
                 .setTitle(`Hypixel Bot Commands`)
                 .setColor(colors['MainColor'])
                 .setDescription(`My Commands\nBot Prefix: \'${prefix}\'`)
-                .addField("Commands:", commands.filter(e => !e.modOnly).filter(e => !e.hiddenCommand).filter(e => !e.notReady).map(command => `**${command.name}**`).join(' '))
+                .addField("Commands:", commands.filter(e => !e.modOnly).filter(e => !e.hiddenCommand).filter(e => !e.notReady).map(command => `**${command.name}** - ${command.description}`).join('\n'))
                 .setFooter(`${message.author.tag} | Created by Gurkirat`, message.author.displayAvatarURL({
                     format: 'png',
                     dynamic: true,
