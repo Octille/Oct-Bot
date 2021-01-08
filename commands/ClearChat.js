9f73aac667f6986496a1155dd07178d35c2eb50b
Gurkirat <sainigurkirat111@gmail.com>
2021-01-08 13:49:11 -0600

    Update ClearChat.js

--- commands/ClearChat.js
+++ commands/ClearChat.js
@@ -1,30 +1,18 @@
-module.exports.run  = async (bot, message) => {
-    const messageArray = message.content.split(' ');
-    const args = messageArray.slice(1);
-
-    if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('Lack of Perms!');
-
-    let deleteAmount;
-
-    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!') }
-
-    if (parseInt(args[0]) > 100) {
-        return message.reply('You can only delete 100 messages at a time!')
-    } else {
-        deleteAmount = parseInt(args[0]);
-    }
-
-    message.channel.bulkDelete(deleteAmount + 1, true);
-    message.reply(`**Successfully** Deleted ***${deleteAmount}*** Messages.`)
-
-}
-
-
-
-
-
 module.exports = {
-    name: 'ClearChat',
-    description: 'C',
-
-}
+    name: 'clearchat',
+    description: "Clear messages!",
+   async  execute(message, args) {
+        if (!args[0]) return message.reply("Please enter the amount of messages to clear!");
+ 
+        if(isNaN(args[0])) return message.reply("Please type a real number!");
+ 
+        if(args[0] > 100) return message.reply("You can't remove more than 100 messages!");
+        
+        if(args[0] < 1) return message.reply("You have to delete at least one message!");
+ 
+        await message.channel.messages.fetch({ limit: args[0]}).then(messages =>{
+            message.channel.bulkDelete(messages)
+    });
+ 
+ }
+} 
