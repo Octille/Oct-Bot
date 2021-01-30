30192ca43dcbfc9f8fc15da5fd3e00d20c433150
Gurkirat <sainigurkirat111@gmail.com>
2021-01-30 15:30:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -38,38 +38,14 @@ client.on('message', message => {
 	if (!client.commands.has(command)) return;
 
 	try {
-		client.commands.get('avatar').execute(message, args, Discord, client);
+	
 	} catch {
 		message.reply('there was an error trying to execute that command!');
 	}
 
 });
 
-client.on('message', message => {
-	if (!message.guild) return;
-	if (message.content.startsWith('!kick')) {
 
-	  if (user) {
-		const member = message.guild.member(user);  
-		if (message.member.hasPermission('KICK_MEMBERS')) {
-			
-		  member
-			.kick('Optional reason that will display in the audit logs')
-			.then(() => {
-			  message.reply(`Successfully kicked ${user.tag}`);
-			})
-			.catch(err => {
-			  message.reply('I was unable to kick the member');
-			  console.error(err);
-			});
-		} else {
-		  message.reply("That user isn't in this guild!");
-		}
-	  } else {
-		message.reply("You didn't mention the user to kick!");
-	  }
-	}
-  });
 
 
 client.login(process.env.token);
