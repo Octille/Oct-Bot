c0e2a4b289faadc17a8b1c76d9f44018a72aa9f3
Gurkirat <sainigurkirat111@gmail.com>
2021-02-02 14:36:03 -0600

    commad handler

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const fetch = require("node-fetch");
+const { prefix, token } = require('./token.json');
+const WOKCommands = require('wokcommands')
 
+client.once('ready', () => {
+	console.log('Ready!')
 
-const { prefix, token } = require('./config.json');
+	new WOKCommands(client, 'commands')
+})
 
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).execute(message, args, Discord, client);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-
-});
-
-
 
 
 client.login(process.env.token);
