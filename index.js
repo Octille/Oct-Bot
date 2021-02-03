8fea02cbd00d53484cd003be7a46efe8adb0b5cd
Gurkirat <sainigurkirat111@gmail.com>
2021-02-03 15:11:24 -0600

    per server prefix

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { Client, Collection } = require("discord.js");
 const { config } = require("dotenv");
 const fs = require("fs");
 const mongoose = require('mongoose');
+const prefix = require('discord-prefix');
+
+
+let defaultPrefix = '!';
 
 const client = new Client({
     disableEveryone: true
@@ -29,14 +33,18 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-    const prefix = "!";
+       //get the prefix for the discord server
+	let guildPrefix = prefix.getPrefix(message.guild.id);
+
+	   //set prefix to the default prefix if there isn't one
+	if (!guildPrefix) guildPrefix = defaultPrefix;
 
     if (message.author.bot) return;
     if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(guildPrefix)) return;
     if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(guildPrefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     
     if (cmd.length === 0) return;
