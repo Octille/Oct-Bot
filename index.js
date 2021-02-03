b23eee6160efa41eaa79887698a00be4ee2cfa02
Gurkirat <sainigurkirat111@gmail.com>
2021-02-03 14:14:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -39,37 +39,6 @@ client.on("ready", () => {
 client.on("message", async message => {
 	client.on('message', async (message) => {
 
-       
-
-
-
-
-
-    const prefix = "!";
-
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    
-    if (cmd.length === 0) return;
-    
-    let command = client.commands.get(cmd);
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-    if (command) 
-        command.run(client, message, args);
-       
-
-
-
-
-
-
-
     if (message.author.bot) return;
 
     //Getting the data from the model
