49d7f4afa15253cd3f9ab9a5a7ab6c62add55082
Gurkirat <sainigurkirat111@gmail.com>
2021-02-03 15:30:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const { Client, Collection } = require("discord.js");
 const { config } = require("dotenv");
 const fs = require("fs");
 const mongoose = require('mongoose');
-const prefix = require('discord-prefix');
+const MongoClient = require('mongodb').MongoClient;
+const MongoDBProvider = require('commando-provider-mongo').MongoDBProvider;
+
+const client = new Commando.CommandoClient({
+	owner: '251120969320497152',
+	commandPrefix: config.prefix,
+  })
+  
+  client.setProvider(
+	MongoClient.connect(config.mongoPath)
+	  .then((client) => {
+		return new MongoDBProvider(client, 'Oct Bot')
+	  })
+	  .catch((err) => {
+		console.error(err)
+	  })
+  )
 
 
-let defaultPrefix = '!';
+
 
 const client = new Client({
     disableEveryone: true
@@ -33,18 +49,13 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-       //get the prefix for the discord server
-	let guildPrefix = prefix.getPrefix(message.guild.id);
-
-	   //set prefix to the default prefix if there isn't one
-	if (!guildPrefix) guildPrefix = defaultPrefix;
 
     if (message.author.bot) return;
     if (!message.guild) return;
-    if (!message.content.startsWith(guildPrefix)) return;
+    if (!message.content.startsWith(prefix)) return;
     if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    const args = message.content.slice(guildPrefix.length).trim().split(/ +/g);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     
     if (cmd.length === 0) return;
