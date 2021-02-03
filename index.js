eb22ed1e7acda340c88f87c26b11704a39cc9d5a
Gurkirat <sainigurkirat111@gmail.com>
2021-02-03 14:21:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -36,34 +36,26 @@ client.on("ready", () => {
     }); 
 });
 
-client.on('message', async (message) => {
+client.on("message", async message => {
+    const prefix = "_";
+
     if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    //Getting the data from the model
-    const data = await prefix.findOne({
-        GuildID: message.guild.id
-    });
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-    const messageArray = message.content.split(' ');
-    const cmd = messageArray[0];
-    const args = messageArray.slice(1);
+    if (command) 
+        command.run(client, message, args);
+});
 
-    //If there was a data, use the database prefix BUT if there is no data, use the default prefix which you have to set!
-    if(data) {
-        const prefix = data.Prefix;
-
-        if (!message.content.startsWith(prefix)) return;
-        const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
-        commandfile.run(client, message, args);
-    } else if (!data) {
-        //set the default prefix here
-        const prefix = "!";
-        
-        if (!message.content.startsWith(prefix)) return;
-        const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
-        commandfile.run(client, message, args);
-    }
-})
 
 
 client.login(process.env.token);
