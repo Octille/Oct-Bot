fc82d5eda50bebd7b30ca2d5758c46ae3dd29ac3
Gurkirat <sainigurkirat111@gmail.com>
2021-02-03 14:10:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -38,34 +38,8 @@ client.on("ready", () => {
 
 client.on("message", async message => {
 	client.on('message', async (message) => {
-		if (message.author.bot) return;
-	
-		//Getting the data from the model
-		const data = await prefix.findOne({
-			GuildID: message.guild.id
-		});
-	
-		const messageArray = message.content.split(' ');
-		const cmd = messageArray[0];
-		const args = messageArray.slice(1);
-	
-		//If there was a data, use the database prefix BUT if there is no data, use the default prefix which you have to set!
-		if(data) {
-			const prefix = data.Prefix;
-	
-			if (!message.content.startsWith(prefix)) return;
-			const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
-			commandfile.run(client, message, args);
-		} else if (!data) {
-			//set the default prefix here
-			const prefix = "!";
-			
-			if (!message.content.startsWith(prefix)) return;
-			const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
-			commandfile.run(client, message, args);
-		}
-	})
 
+       
 
 
 
@@ -88,6 +62,42 @@ client.on("message", async message => {
 
     if (command) 
         command.run(client, message, args);
+       
+
+
+
+
+
+
+
+    if (message.author.bot) return;
+
+    //Getting the data from the model
+    const data = await prefix.findOne({
+        GuildID: message.guild.id
+    });
+
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
+    const args = messageArray.slice(1);
+
+    //If there was a data, use the database prefix BUT if there is no data, use the default prefix which you have to set!
+    if(data) {
+        const prefix = data.Prefix;
+
+        if (!message.content.startsWith(prefix)) return;
+        const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+        commandfile.run(client, message, args);
+    } else if (!data) {
+        //set the default prefix here
+        const prefix = "!";
+        
+        if (!message.content.startsWith(prefix)) return;
+        const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+        commandfile.run(client, message, args);
+    }
+})
+
 });
 
 client.login(process.env.token);
