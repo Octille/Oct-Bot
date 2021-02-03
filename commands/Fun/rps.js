02e731967b232fdc1c08fdfb5303b384cdd8ed22
Gurkirat <sainigurkirat111@gmail.com>
2021-02-03 12:04:53 -0600

    Update rps.js

--- commands/Fun/rps.js
+++ commands/Fun/rps.js
@@ -9,7 +9,7 @@ module.exports = {
     description: "Rock Paper Scissors game. React to one of the emojis to play the game.",
     usage: "rps",
     run: async (client, message, args) => {
-        const embed = new RichEmbed()
+        const embed = new Discord.MessageEmbed()
             .setColor("#ffffff")
             .setFooter(message.guild.me.displayName, client.user.displayAvatarURL)
             .setDescription("Add a reaction to one of these emojis to play the game!")
