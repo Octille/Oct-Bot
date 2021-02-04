65824a4b5124b803a3ee134e4e75bfbad1435083
Gurkirat <sainigurkirat111@gmail.com>
2021-02-03 19:07:44 -0600

    Update help.js

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -1,35 +1,57 @@
-const Discord = require('discord.js');
-const colors = require('../../colors.json');
-const {
-    prefix
-
-} = require('../../config.json');
+const discord = require('discord.js');
+const { stripIndents } = require("common-tags");
 
 module.exports = {
-    name: 'help',
-    description: 'Sends This Very Helpful Message!',
-    async execute(message, args, client) {
-
-        const {
-            commands
-        } = message.client;
-
-        if (!args.length) {
-            const embed = new Discord.MessageEmbed()
-                .setTitle(`Hypixel Bot Commands`)
-                .setColor(colors['MainColor'])
-                .setDescription(`My Commands\nBot Prefix: \'${prefix}\'`)
-                .addField("Commands:", commands.filter(e => !e.modOnly).filter(e => !e.hiddenCommand).filter(e => !e.notReady).map(command => `**${command.name}** - ${command.description}`).join('\n'))
-                .setFooter(`${message.author.tag} | Created by Gurkirat`, message.author.displayAvatarURL({
-                    format: 'png',
-                    dynamic: true,
-                    size: 2048
-                }))
-
-            return message.channel.send(embed);
+    name: "help",
+    aliases: ["h"],
+    category: "info",
+    description: "Returns all commands, or one specific command info",
+    usage: "[command | alias]",
+    run: async (client, message, args) => {
+        if (args[0]) {
+            return getCMD(client, message, args[0]);
+        } else {
+            return getAll(client, message);
         }
-
-    },
-    help: 'Sends this message!'
+    }
 }
 
+function getAll(client, message) {
+    const embed = new discord.MessageEmbed()
+        .setColor("RANDOM")
+
+    const commands = (category) => {
+        return client.commands
+            .filter(cmd => cmd.category === category)
+            .map(cmd => `- \`${cmd.name}\``)
+            .join("\n");
+    }
+
+    const info = client.categories
+        .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
+        .reduce((string, category) => string + "\n" + category);
+
+    return message.channel.send(embed.setDescription(info));
+}
+
+function getCMD(client, message, input) {
+    const embed = new discord.MessageEmbed()
+
+    const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase()));
+    
+    let info = `No information found for command **${input.toLowerCase()}**`;
+
+    if (!cmd) {
+        return message.channel.send(embed.setColor("RED").setDescription(info));
+    }
+
+    if (cmd.name) info = `**Command name**: ${cmd.name}`;
+    if (cmd.aliases) info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
+    if (cmd.description) info += `\n**Description**: ${cmd.description}`;
+    if (cmd.usage) {
+        info += `\n**Usage**: ${cmd.usage}`;
+        embed.setFooter(`Syntax: <> = required, [] = optional`);
+    }
+
+    return message.channel.send(embed.setColor("GREEN").setDescription(info));
+}
