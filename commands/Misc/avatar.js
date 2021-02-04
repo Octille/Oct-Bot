4070208cd80fb6f409ec5591d0cc02b912235102
Gurkirat <sainigurkirat111@gmail.com>
2021-02-04 17:58:33 -0600

    Update avatar.js

--- commands/Misc/avatar.js
+++ commands/Misc/avatar.js
@@ -4,14 +4,10 @@ module.exports = {
     name: "avatar",
     description: "Brodcast someone's avatar",
 
-    async run (client, message, args) {
-        let user = message.mentions.users.first();
-        if(!user) user = message.author;
-        let color = message.member.displayHexColor;
-        if (color == '#000000') color = message.member.hoistRole.hexColor;
-        const embed = new Discord.RichEmbed()
+    run: async (client, message, args) => {
+          const embed = new Discord.RichEmbed()
                         .setImage(user.avatarURL)
-                        .setColor(color)
+                        .setColor("RANDOM")
          message.channel.send({embed});
       }
 }
