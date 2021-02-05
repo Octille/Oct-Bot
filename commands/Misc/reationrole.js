f88380169010994ebde84afb671adc1430eb3e39
Gurkirat <sainigurkirat111@gmail.com>
2021-02-04 19:01:19 -0600

    Update reationrole.js

--- commands/Misc/reationrole.js
+++ commands/Misc/reationrole.js
@@ -1,41 +1,15 @@
+const discord = require('discord.js');
+ 
  module.exports = {
-    name: 'reactionrole',
+    name: 'Verifi',
     description: "Sets up a reaction role message!",
     run: async (client, message, args) => {
-        if(message.member.roles.cache.has('627526794463150100')){
-            message.delete()
-            const channel = '786657488304341023';
-            const Nons = message.guild.roles.cache.find(role => role.name === "Nons");
-     
-            const VerifiEmoji = '✅';
-           
-            let embed = new Discord.MessageEmbed()
-                .setColor('#e42643')
-                .setTitle('Verification!')
-                .setDescription('To get access the server please verfi!\n\n'
-                 + `*To verifi react with check mark emoji* ${VerifiEmoji}`);
-     
-            let messageEmbed = await message.channel.send(embed);
-            messageEmbed.react(VerifiEmoji);
-    
-            client.on('messageReactionAdd', async (reaction, user) => {
-                if (reaction.message.partial) await reaction.message.fetch();
-                if (reaction.partial) await reaction.fetch();
-                if (user.bot) return;
-                if (!reaction.message.guild) return;
-     
-                if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji.name === VerifiEmoji) {
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(Nons);
-                    }
-                } else {
-                    return;
-                }
-     
-            });
+    message.delete()
+    const embed = new discord.MessageEmbed()
+    .setTitle("Verification")
+    .setDescription("React to the check mark emoji to verifi \n \n please read the rules befor verifing")
+    const msgEmbed = await message.channel.send(embed)
+    msgEmbed.react('✅')
 
-        } else {
-            message.channel.send('You don\'t have permissions for this command')
-        }
-    }
 }
+ }
