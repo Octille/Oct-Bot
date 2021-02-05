119b5b56c63de37afe2498ead001a22e7a6b1a4a
Gurkirat <sainigurkirat111@gmail.com>
2021-02-04 19:51:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 const config = require('./config.json')
 const fs = require("fs");
-const { client } = require("discord.js");
+
 
 
 
