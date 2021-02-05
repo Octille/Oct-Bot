44bdd3f8e2e0efd993dc649a9987657540a8f950
Gurkirat <sainigurkirat111@gmail.com>
2021-02-04 20:05:26 -0600

    das
    das

---
+++ commands/Misc/pole.js
@@ -0,0 +1,21 @@
+const discord = require('discord.js');
+ 
+ module.exports = {
+    name: 'pole',
+    description: "Sets up a pole!",
+    run: async (client, message, args) => {
+        let pollChannel = message.mentions.channels.first();
+        let pollDescription = args.slice(1).join(' ');
+
+        let embedPoll = new Discord.MessageEmbed()
+        .setTitle('pole created')
+        .setDescription(pollDescription)
+        .setColor('YELLOW')
+        let msgEmbed = await pollChannel.send(embedPoll);
+        await msgEmbed.react('👍')
+        await msgEmbed.react('👎')
+    }
+
+}
+
+
