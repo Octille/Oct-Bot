d7d854ad4726bf1fdaeb16748a6edac734af130b
Gurkirat <sainigurkirat111@gmail.com>
2021-02-06 14:25:13 -0600

    avatar

--- commands/Misc/avatar.js
+++ commands/Misc/avatar.js
@@ -1,14 +1,30 @@
-const discord = require('discord.js');
-
+const discord = require('discord.js')
 
 module.exports = {
-    name: "avatar",
-    description: "Brodcast someone's avatar",
+  name: 'avatar',
+  aliases: ['icon', 'pfp', 'profilepic'],
+  description: 'Return a user(s) avatar picture!',
 
-    run: async (client, message, args) => {
-          const embed = new discord.MessageEmbed()
-                        .setImage(`${message.member.avatarURL()}`)
-                        .setColor(`RANDOM`)
-         message.channel.send(embed);
+  run: async (client, message, args) => {
+
+    const embed = discord.MessageEmbed()
+    .setTitle('Your Avatar')
+    .setdescription(`${message.author.displayAvatarURL({ dynamic: true })}`)
+
+    const embed1 = discord.MessageEmbed()
+    .setTitle(`${user.username}'s Avatar`)
+    .setdescription(`${user.displayAvatarURL({ dynamic: true })}`)
+
+
+  
+
+      if (!message.mentions.users.size) {
+          return message.channel.send(embed);
       }
+
+      const avatar_list = message.mentions.users.map(user => {
+          return message.channel.send(embed1);
+      });
+
+  }
 }
---
+++ commands/Moderation/kick.js
@@ -0,0 +1,24 @@
+const discord = require('discord.js')
+
+module.exports = {
+    name: 'kick',
+    description: "This command kicks a member!",
+    execute(message, args){
+        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.sned('You Need Permistions To Run This Command!')
+
+        const embed = new discord.MessageEmbed()
+        .setcolor("RANDOM")
+        .settitle('Member Kicked')
+        .setDescription(`I have successfully kicked ${user.username}`)
+
+
+        const target = message.mentions.users.first();
+        if(target){
+            const memberTarget = message.guild.members.cache.get(target.id);
+            memberTarget.kick();
+            message.channel.send(embed);
+        }else{
+            message.channel.send(`You coudn't kick that member!`);
+        }
+    }
+}
