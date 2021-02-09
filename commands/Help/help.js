0d1d5a56c20c303267fafedafd5ecb3333b4c85f
Gurkirat <sainigurkirat111@gmail.com>
2021-02-09 15:16:35 -0600

    sad

--- commands/Help/help.js
+++ commands/Help/help.js
@@ -7,16 +7,13 @@ module.exports = {
     name: 'help',
     description: 'send this message',
     run: async (client, message, args) => {
-        const embed = new discord.MessageEmbed()
-        .setTitle('Type !h "(command name)" for more info')
-        message.channel.send(embed)
         var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
         var fff;
         var hmu = {};
         fs.readdir("./commands", (err2, fff) => {
             for (i = 0; i < fff.length; i++) {
                 hmu[i] = new discord.MessageEmbed();
-                hmu[i].setTitle('Type !h "(command name)" for more info \n' + fff[i]);
+                hmu[i].setTitle('`***Type !h "(command name)" for more info*** \n`' + fff[i]);
                 hmu[i].setColor(config.embedColor);
                 const iii = i;
                 fs.readdir(`./commands/${fff[i]}/`, (err1, files1) => {
