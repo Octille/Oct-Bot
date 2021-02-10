2dd9574f62cd967f74c16dc6d00b9880ae9a5565
Gurkirat <sainigurkirat111@gmail.com>
2021-02-10 11:47:00 -0600

    Update ping.js

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: "this is a test command!",
     run: async (client, message, args) =>{
-        message.channel.send('pong!');
-    }
-}
+        message.channel.send(`🏓 | Latency is: **${Date.now() - message.createdTimestamp}ms.**`);
+    },
+};
