495ef62ffd4779fc0615875668debfa5f7e31eec
Gurkirat <sainigurkirat111@gmail.com>
2021-02-10 11:52:35 -0600

    Update ping.js

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -1,7 +1,13 @@
+const discord = require('discord.js');
+
 module.exports = {
     name: 'ping',
     description: "this is a test command!",
     run: async (client, message, args) =>{
-        message.channel.send(`🏓 | Latency is: **${Date.now() - message.createdTimestamp}ms.**`);
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`🏓 | Latency is: **${Date.now() - message.createdTimestamp}ms.**`)
+
+        
+        message.channel.send(embed);
     },
 };
