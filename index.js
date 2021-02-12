564023beb2c592bd44e0ba820811813f464270a5
Gurkirat <sainigurkirat111@gmail.com>
2021-02-12 10:18:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { default_prefix, config } = require('./config.json');
 const fs = require("fs");
 const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 
-bot.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', member => {
   member.guild.channels.get('809519949193805845').send(`${member} Welcome to the OCT server! please read #rules to continue `); 
 });
 
