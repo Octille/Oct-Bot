31f7af31284a41612babb7bd689b0318de47b526
Gurkirat <sainigurkirat111@gmail.com>
2021-02-20 12:36:12 -0600

    s

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -3,9 +3,9 @@ const Discord = require('discord.js')
 
 module.exports = (client, discord) =>{
     const load_dir = (dirs) =>{
-    const event_files = fs.readdirSync(`./event/${dirs}`).filter(file => file.endsWith('.js'));
+    const event_files = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
     for(const file of event_files){
-        const event = require(`../event/${dirs}/${file}`);
+        const event = require(`../events/${dirs}/${file}`);
         const event_name = file.split('.')[0];
         client.on(event_name, event.bind(null, Discord, client));
     }
