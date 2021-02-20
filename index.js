1c8403665cb4125b5bef66faf414d0a00c2c01ac
Gurkirat <sainigurkirat111@gmail.com>
2021-02-20 12:08:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,17 +20,24 @@ fs.readdir("./events/", (err, files) => {
   });
 
 
-fs.readdir(`./commands/${category}`, (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`../commands/${category}/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Loaded ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-
+  const { Collection } = require("discord.js");
+  bot.commands = new Collection();
+  
+  //We let the bot read through the 'commands' folder and return an array including all category folders
+  const categories = fs.readdirSync('./commands/');
+  
+  for (const category of categories) {
+  const commandFiles = fs.readdirSync(`./commands/${category}`).filter(File => File.endsWith('.js'));
+  //We now enter every sub-folder one by one and filter the files to include .js only, readdirSync() returns an array including the items/files in that directory 
+  
+  //We create an intended for loop (notice how the for loops are inside eachother)
+  for (const file of commandFiles) {
+    const command = require(`../commands/${category}/${file}`);
+    //We grab that command-file and it's values, and we push it into the commands collection
+  
+    bot.commands.set(command.name, command);
+          }
+      }
 
 
 
