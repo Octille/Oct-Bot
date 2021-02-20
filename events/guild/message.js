6164ce6ddcde9ae22a5e38ebd5635c68b02e3428
Gurkirat <sainigurkirat111@gmail.com>
2021-02-20 15:08:16 -0600

    x

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,26 +1,8 @@
-const profileModel = require("../../models/profileSchema");
 
 
-module.exports = async (Discord, client, message) => {
+module.exports =(Discord, client, message) => {
     const prefix = '!';
     if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-  let profileData;
-  try {
-    profileData = await profileModel.findOne({ userID: message.author.id });
-    if (!profileData) {
-      let profile = await profileModel.create({
-        userID: message.author.id,
-        serverID: message.guild.id,
-        coins: 1000,
-        bank: 0,
-      });
-      profile.save();
-    }
-  } catch (err) {
-    console.log(err);
-  }
-  
     
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
