5be3c3be3993ac4adefae0449952f8b60164ce9c
Gurkirat <sainigurkirat111@gmail.com>
2021-03-03 20:32:14 -0600

    Update inventory.js

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -8,8 +8,8 @@ module.exports = {
         const Cookies = profileData.Items.Cookies
         const inventory1 = new Discord.MessageEmbed()
         .setTitle(`Items Owned`)
-        .addField(`<:FishingRod:816342491111882782>Fishing Rod - ${FishingRod.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`, `ID: \`FishingRod\``)
-        .addField(`:cookie:Cookies - ${Cookies.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`, `ID: \`Cookies\``)
+        .addField(`<:FishingRod:816342491111882782>Fishing Rod - ${profileData.Items.FishingRod}`, `ID: \`FishingRod\``)
+        .addField(`:cookie:Cookies - ${profileData.Items.Cookies}`, `ID: \`Cookies\``)
 
         if(!args[0]){
             return message.channel.send(inventory1)
